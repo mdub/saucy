4f46c3c1a467c569f6d8bea29d0057367a3a877b
Mike Williams <mdub@dogbiscuit.org>
2018-02-12 17:02:42 +1100

    Start writing to the database.

--- lib/saucy/db/event_store.rb
+++ lib/saucy/db/event_store.rb
@@ -13,8 +13,31 @@ module Saucy
         @db = db
       end
 
+      attr_reader :db
+
       def get_commits_on(stream_id)
         []
+      end
+
+      def commit_event_on(stream_id, event)
+        new_version = 1
+        event_streams.insert("stream_id" => stream_id, "current_version" => new_version)
+        commit = {
+          "stream_id" => stream_id,
+          "version" => 1,
+          "event" => event
+        }
+        commit
+      end
+
+      private
+
+      def event_streams
+        db[:event_streams]
+      end
+
+      def events
+        db[:events]
       end
 
     end
--- spec/saucy/db/event_store_spec.rb
+++ spec/saucy/db/event_store_spec.rb
@@ -13,6 +13,19 @@ describe Saucy::DB::EventStore do
     Saucy::DB::EventStore.new(db)
   end
 
+  around(:each) do |example|
+    db.transaction do
+      example.run
+      raise Sequel::Rollback
+    end
+  end
+
+  before(:each) do
+    if ENV["DB_DEBUG"]
+      db.loggers << Logger.new($stdout)
+    end
+  end
+
   context "a non-extant stream" do
 
     let(:stream_id) { SecureRandom.uuid }
@@ -21,6 +34,23 @@ describe Saucy::DB::EventStore do
 
       it "returns an empty list" do
         expect(event_store.get_commits_on("whatever").to_a).to eq([])
+      end
+
+    end
+
+    describe "#commit_event_on" do
+
+      let(:event) { {"foo" => "bar"} }
+
+      let!(:result) { event_store.commit_event_on(stream_id, event) }
+
+      it "adds a row to event_streams" do
+        event_stream_entry = db[:event_streams].where(:stream_id => stream_id).first!
+        expect(event_stream_entry).to include(:stream_id => stream_id, :current_version => 1)
+      end
+
+      it "returns the new version number" do
+        expect(result.fetch("version")).to eq(1)
       end
 
     end
