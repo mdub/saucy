ac195214bc85766cde3fd9724bd5137e6b8b912d
Mike Williams <mdub@dogbiscuit.org>
2018-02-14 07:38:59 +1100

    Much saucier.

--- lib/saucy/calc.rb
+++ lib/saucy/calc.rb
@@ -3,21 +3,58 @@ module Saucy
   class Calc
 
     def initialize
-      @value = 0
+      @state = State.new
     end
 
-    attr_reader :value
+    def value
+      state.value
+    end
 
     def add(arg)
-      @value += Float(arg)
+      apply(:add, arg)
     end
 
     def set(arg)
-      @value = Float(arg)
+      apply(:set, arg)
     end
 
     def clear
-      set(0)
+      apply(:clear)
+    end
+
+    private
+
+    attr_reader :state
+
+    def apply(*change)
+      @state = @state.apply(*change)
+    end
+
+    class State
+
+      def initialize(value = 0)
+        @value = value
+      end
+
+      attr_reader :value
+
+      def apply(op, *args)
+        raise "Unrecognised operation: #{op.type}" unless respond_to?(op)
+        send(op, *args)
+      end
+
+      def add(arg)
+        State.new(value + Float(arg))
+      end
+
+      def set(arg)
+        State.new(Float(arg))
+      end
+
+      def clear
+        set(0)
+      end
+
     end
 
   end
