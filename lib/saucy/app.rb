6f6706ccbb87426b1e795605f6ba0160d12785aa
Mike Williams <mdub@dogbiscuit.org>
2018-03-09 17:27:07 +1100

    Streaming example.

--- lib/saucy/app.rb
+++ lib/saucy/app.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
 
+class Clock
+  def each
+    loop do
+      yield "#{Time.now}\n"
+      sleep 1
+    end
+  end
+end
+
+get('/clock') do
+  content_type :json
+  Clock.new
+end
+
 get '/' do
   'Hello world!'
 end
